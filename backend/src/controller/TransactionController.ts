1739c38f901ec9058a3fd18c3d21dcd3ecb9edbb
agarwalmanya <manya18ag@gmail.com>
2020-11-10 16:31:19 +0800

    server: add sell sharewon and transaction history

--- backend/src/controller/TransactionController.ts
+++ backend/src/controller/TransactionController.ts
@@ -2,9 +2,7 @@ import { getRepository, getConnection } from "typeorm";
 import { NextFunction, Request, Response } from "express";
 import { Transaction } from "../entity/Transaction";
 import { stripe, stripePublicKey } from "../payments";
-import { raw } from "body-parser";
-import { stat } from "fs";
-
+import fetch from "node-fetch";
 export class TransactionController {
   private transactionRepository = getRepository(Transaction);
 
@@ -53,7 +51,7 @@ export class TransactionController {
     }
   }
 
-   /*
+  /*
          @params request.body {
            paymentIntentId,
            status (1 for success, 2 for fail),
@@ -67,28 +65,66 @@ export class TransactionController {
     next: NextFunction
   ) {
     const { paymentIntentId, status } = request.body;
-    const transactionAddress = status == 1 ? request.body.transaction_address : ""
-    let transactionToUpdate = await this.transactionRepository.findOne({stripe_id: paymentIntentId})
-    transactionToUpdate.status = status
-    transactionToUpdate.transaction_address = transactionAddress
-    return this.transactionRepository.save(transactionToUpdate)
+    const transactionAddress =
+      status == 1 ? request.body.transaction_address : "";
+    let transactionToUpdate = await this.transactionRepository.findOne({
+      stripe_id: paymentIntentId,
+    });
+    transactionToUpdate.status = status;
+    transactionToUpdate.transaction_address = transactionAddress;
+    return this.transactionRepository.save(transactionToUpdate);
   }
 
+  /* 
+    @params request.body { 
+      admin_address, 
+      user_address,
+      timestamp 
+    }
+  */
   async sell(request: Request, response: Response, next: NextFunction) {
-    const { amount, currency, transaction_address, user_id } = request.body;
-    // Create an intent to transfer money to user
-    const transaction = {
-      user_id: user_id,
-      type: 1,
-      status: 0,
-      stripe_id: "",
-      transaction_address: transaction_address,
-      createdAt: new Date(),
-    };
+    const { admin_address, user_address, timestamp } = request.body;
+    const transactionsRes = await fetch(
+      `https://apilist.tronscan.org/api/trc10trc20-transfer?sort=-timestamp&count=true&start=0&total=0&direction=&address=${user_address}`
+    );
+    const data = transactionsRes.json();
+    const transfers = data["transfers"];
+    const toAdminTransfers = transfers.filter(
+      (transfer) =>
+        transfer["to_address"] == admin_address &&
+        transfer["timestamp"] == timestamp &&
+        transfer["confirmed"] == true
+    );
+    if(toAdminTransfers.length > 0){ 
+      //start process to send money to user's stripe account 
+       
+    } else { 
+      response.send({ 
+        message: 'Sell Transaction not found'
+      })
+    }
+  }
 
-    // Send publishable key and PaymentIntent details to client
-    return this.transactionRepository.save(transaction);
-    
+  /*
+    request.body{ 
+      type: 0 or 1, 
+      user_id
+    } 
+  */
+  async transactionsHistory(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    const { type, user_id } = request.body;
+    const transactions = this.transactionRepository
+      .createQueryBuilder("transaction")
+      .where("transaction.user_id = :id AND type = :type", {
+        id: user_id,
+        type: type,
+      })
+      .getMany();
+    response.send({transactions})
   }
 
   async remove(request: Request, response: Response, next: NextFunction) {
